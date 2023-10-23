69e87dddd7a774a878233f72b1b541ceb783c3b3
torinunna <torinunna9588@gmail.com>
2023-10-23 17:20:27 +0900

    add tableView and addBtnPressed method

---
+++ TodoList/Task.swift
@@ -0,0 +1,13 @@
+//
+//  Task.swift
+//  TodoList
+//
+//  Created by YUJIN KWON on 2023/10/23.
+//
+
+import Foundation
+
+struct Task {
+    var title: String
+    var done: Bool
+}
--- TodoList/ViewController.swift
+++ TodoList/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    var tasks = [Task]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.tableView.dataSource = self
     }
 
-
+    @IBAction func editBtnPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func addBtnPressed(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "할 일 추가", message: nil, preferredStyle: .alert)
+        let addButton = UIAlertAction(title: "추가", style: .default, handler: { [weak self] _ in
+            guard let title = alert.textFields?[0].text else { return }
+            let task = Task(title: title, done: false)
+            self?.tasks.append(task)
+            self?.tableView.reloadData()
+        })
+        let cancelButton = UIAlertAction(title: "취소", style: .cancel)
+        alert.addAction(cancelButton)
+        alert.addAction(addButton)
+        alert.addTextField(configurationHandler: { textfield in
+            textfield.placeholder = "할 일을 입력해주세요"
+        })
+        self.present(alert, animated: true)
+    }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let task = self.tasks[indexPath.row]
+        cell.textLabel?.text = task.title
+        return cell
+    }
+}
